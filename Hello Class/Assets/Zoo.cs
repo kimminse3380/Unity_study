d9af93eeec26266d600fbe48996ea5b2a3619bb9
kimminse3380 <mss2214@naver.com>
2023-08-29 00:16:27 +0900

    20230829

---
+++ Hello Class/Assets/Zoo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animal tom = new Animal();
+        tom.name = "ล่";
+        tom.sound = "fuck!";
+
+        tom.PlaySound();
+    }
+}
